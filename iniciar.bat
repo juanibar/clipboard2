a9c1c4ccc8d2cbccf7ff6a72971e8d32d1c2da90
Juan Ignacio <juanibarrientos.fiuba@gmail.com>
2026-09-04 21:51:15 -0300

    push1

--- iniciar.bat
+++ iniciar.bat
@@ -2,5 +2,17 @@
 :: Cambia el directorio actual a la carpeta donde está este archivo
 cd /d "%~dp0"
 
-:: Ejecuta el script de Python
+:: 1. DESCARGAR CAMBIOS (Pull antes de abrir)
+git pull
+
+:: 2. EJECUTAR LA APLICACIÓN
+:: (La consola invisible se quedará esperando en este paso hasta que cierres la app)
 python clipboard_app.py
+
+:: 3. SUBIR CAMBIOS AL CERRAR LA APP
+:: Agrega solo el archivo de la base de datos
+git add mensajes_clipboard.csv
+:: Crea un commit automático
+git commit -m "Auto-sync: Actualizacion de mensajes"
+:: Sube los cambios
+git push
